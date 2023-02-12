c4f0864d35574c832c26f00f035616c98e84b2a2
Kealan McGuinness <kealanmcguimmess@Kealans-MacBook-Pro.local>
2023-02-12 17:46:11 +0000

    Week 02 weekly tast new with division

--- bank.py
+++ bank.py
@@ -3,13 +3,14 @@
 # Week02 task 02 banks
 # int to convert string into an integer
 # use formattedfloat to change numbers into euro
-# I haven't figured out how to have the Euro value appear correctly yet. Will return to this.
+
 
 x = input("Enter first amount (in cent): ")
 y = input("Enter second amount (in cent): ")
 amount = int(x) + int(y)
+newamount = amount/100
 
-formattedfloat = str("The sum is: €{:,.2f}".format(amount))
+formattedfloat = str("The sum is: €{:,.2f}".format(newamount))
 
 print(formattedfloat)
 
